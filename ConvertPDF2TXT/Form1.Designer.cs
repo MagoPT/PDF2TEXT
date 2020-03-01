6cecfccd47c222e24c13fc85d2eeb68cb38eb986
Duarte Cruz <duartemcruz@hotmail.com>
2020-03-01 16:35:10 +0000

    V1.4
    Leitura de multiplos ficheiros
    Armazenamento dos ficheiros em tabela

--- ConvertPDF2TXT/Form1.Designer.cs
+++ ConvertPDF2TXT/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
@@ -41,6 +40,18 @@
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cod = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.n_guia = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quant = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.data = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.matricula = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // richTextBox1
@@ -50,13 +61,6 @@
             this.richTextBox1.Size = new System.Drawing.Size(627, 273);
             this.richTextBox1.TabIndex = 0;
             this.richTextBox1.Text = "";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(12, 305);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(526, 20);
-            this.textBox1.TabIndex = 1;
             // 
             // button1
             // 
@@ -95,7 +99,7 @@
             // textBox2
             // 
             this.textBox2.Enabled = false;
-            this.textBox2.Location = new System.Drawing.Point(53, 331);
+            this.textBox2.Location = new System.Drawing.Point(53, 293);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(55, 20);
             this.textBox2.TabIndex = 5;
@@ -103,7 +107,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 334);
+            this.label1.Location = new System.Drawing.Point(0, 296);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 6;
@@ -112,7 +116,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 360);
+            this.label2.Location = new System.Drawing.Point(9, 322);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(35, 13);
             this.label2.TabIndex = 8;
@@ -121,7 +125,7 @@
             // textBox3
             // 
             this.textBox3.Enabled = false;
-            this.textBox3.Location = new System.Drawing.Point(53, 357);
+            this.textBox3.Location = new System.Drawing.Point(53, 319);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(55, 20);
             this.textBox3.TabIndex = 7;
@@ -129,7 +133,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(114, 334);
+            this.label3.Location = new System.Drawing.Point(114, 296);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(35, 13);
             this.label3.TabIndex = 10;
@@ -138,16 +142,110 @@
             // textBox4
             // 
             this.textBox4.Enabled = false;
-            this.textBox4.Location = new System.Drawing.Point(164, 331);
+            this.textBox4.Location = new System.Drawing.Point(164, 293);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(60, 20);
             this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(164, 319);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(112, 20);
+            this.textBox5.TabIndex = 13;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(114, 322);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "label4";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Enabled = false;
+            this.textBox6.Location = new System.Drawing.Point(271, 293);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(66, 20);
+            this.textBox6.TabIndex = 15;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(230, 296);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "label5";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.cod,
+            this.n_guia,
+            this.quant,
+            this.data,
+            this.matricula});
+            this.dataGridView1.Location = new System.Drawing.Point(646, 13);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(543, 370);
+            this.dataGridView1.TabIndex = 16;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
+            // 
+            // cod
+            // 
+            this.cod.HeaderText = "Cod. Ler";
+            this.cod.Name = "cod";
+            this.cod.ReadOnly = true;
+            // 
+            // n_guia
+            // 
+            this.n_guia.HeaderText = "Numero da Guia";
+            this.n_guia.Name = "n_guia";
+            this.n_guia.ReadOnly = true;
+            // 
+            // quant
+            // 
+            this.quant.HeaderText = "Quantidade";
+            this.quant.Name = "quant";
+            this.quant.ReadOnly = true;
+            // 
+            // data
+            // 
+            this.data.HeaderText = "Data de entrada";
+            this.data.Name = "data";
+            this.data.ReadOnly = true;
+            // 
+            // matricula
+            // 
+            this.matricula.HeaderText = "Matricula";
+            this.matricula.Name = "matricula";
+            this.matricula.ReadOnly = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(343, 292);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(215, 95);
+            this.listBox1.TabIndex = 17;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(653, 395);
+            this.ClientSize = new System.Drawing.Size(1202, 395);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -157,11 +255,11 @@
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -170,7 +268,6 @@
         #endregion
 
         private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
@@ -182,6 +279,17 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cod;
+        private System.Windows.Forms.DataGridViewTextBoxColumn n_guia;
+        private System.Windows.Forms.DataGridViewTextBoxColumn quant;
+        private System.Windows.Forms.DataGridViewTextBoxColumn data;
+        private System.Windows.Forms.DataGridViewTextBoxColumn matricula;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- ConvertPDF2TXT/Form1.cs
+++ ConvertPDF2TXT/Form1.cs
@@ -25,20 +25,25 @@ namespace ConvertPDF2TXT
             button1.Text = "Abrir PDF";
             button2.Text = "Converter";
             button3.Text = "Salvar";
-            label1.Text = "ID:";
+            label1.Text = "Cd. LER:";
             label2.Text = "Peso:";
             label3.Text = "Matricula:";
-            textBox1.Enabled = false;
+            label4.Text = "Cd Doc:";
+            label5.Text = "Data:";
+            //textBox1.Enabled = false;
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
             OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Multiselect = true;
             ofd.Filter = "Ficheiros PDF (*.pdf)|*.pdf";
             if (ofd.ShowDialog() == DialogResult.OK)
             {
-                string path = ofd.FileName.ToString();
-                textBox1.Text = path;
+                foreach(string item in ofd.FileNames)
+                {
+                    listBox1.Items.Add(item);
+                }
             }
           
         }
@@ -46,75 +51,92 @@ namespace ConvertPDF2TXT
         public string anterior;
         private void Button2_Click(object sender, EventArgs e)
         {
-            PDDocument doc = PDDocument.load(textBox1.Text);
-            PDFTextStripper stripper = new PDFTextStripper();
-            richTextBox1.Text = (stripper.getText(doc));
-            string sPattern = "^\\d{6}$";
-            int contador = 0;
-            bool cod_ler;
-            int matricula_str =0;
-            textBox2.Text = "Error";
-            textBox3.Text = "Error";
-            textBox4.Text = "Error";
-            foreach (string s in richTextBox1.Lines)
+            foreach(string item in listBox1.Items)
             {
-                
-                try
+                PDDocument doc = PDDocument.load(item);
+                PDFTextStripper stripper = new PDFTextStripper();
+                richTextBox1.Text = (stripper.getText(doc));
+                string sPattern = "^\\d{6}$";
+                int contador = 0;
+                bool cod_ler;
+                int matricula_str = 0;
+                textBox2.Text = "Error";
+                textBox3.Text = "Error";
+                textBox4.Text = "Error";
+                textBox5.Text = "Error";
+                textBox6.Text = "Error";
+                foreach (string s in richTextBox1.Lines)
                 {
-                    if (matricula_str == 1)
-                    {
-                        matricula_str++;
-                        string matricula = new string(s.Reverse().ToArray());
-                        matricula = new string(matricula.Substring(0, 25).Reverse().ToArray());
-                        textBox4.Text = matricula.Substring(0, 8);
-                    }
 
-                    if (s.Contains("N.º ORDEM NIF/NIPC ORGANIZAÇÃO MATRÍCULA DATA INÍCIO TRANSPORTE HORA INÍCIO TRANSPORTE"))
+                    try
                     {
-                        matricula_str++;
-                    }
+                        if (matricula_str == 1)
+                        {
+                            matricula_str++;
+                            string matricula = new string(s.Reverse().ToArray());
+                            matricula = new string(matricula.Substring(0, 25).Reverse().ToArray());
+                            textBox4.Text = matricula.Substring(0, 8);
+                            textBox6.Text = matricula.Substring(9, 10);
 
-                    if (contador == 0)
-                    {
-                        if (s.Contains("DADOS ORIGINAIS"))
+                        }
+
+                        if (s.Contains("N.º ORDEM NIF/NIPC ORGANIZAÇÃO MATRÍCULA DATA INÍCIO TRANSPORTE HORA INÍCIO TRANSPORTE"))
+                        {
+                            matricula_str++;
+                        }
+
+                        if (s.Contains("CÓDIGO DOCUMENTO"))
+                        {
+                            textBox5.Text = s.Substring(17, 16);
+                        }
+
+                        if (contador == 0)
+                        {
+                            if (s.Contains("DADOS ORIGINAIS"))
+                            {
+                                contador++;
+                            }
+                        }
+                        else
                         {
                             contador++;
-                        }
-                    }
-                    else
-                    {
-                        contador++;
-                        if (contador >= 1)
-                        {
+                            if (contador >= 1)
+                            {
 
-                            try
-                            {
-                                Console.WriteLine(s);
-                                cod_ler = true;
-                                string teste_str = s.Substring(0, 6);
-                                int teste = int.Parse(teste_str);
-                                textBox2.Text = s.Substring(0, 6);
-                                textBox3.Text = anterior.Substring(0, 7);
-                                break;
-                            }
-                            catch (Exception ex)
-                            {
                                 try
                                 {
-                                    string teste_str = s.Substring(0, 7);
-                                    float teste = float.Parse(teste_str);
-                                    anterior = s;
+                                    Console.WriteLine(s);
+                                    cod_ler = true;
+                                    string teste_str = s.Substring(0, 6);
+                                    int teste = int.Parse(teste_str);
+                                    textBox2.Text = s.Substring(0, 6);
+                                    textBox3.Text = anterior.Substring(0, 7);
+                                    break;
                                 }
-                                catch { }
+                                catch (Exception ex)
+                                {
+                                    try
+                                    {
+                                        string teste_str = s.Substring(0, 7);
+                                        float teste = float.Parse(teste_str);
+                                        anterior = s;
+                                    }
+                                    catch { }
 
+                                }
                             }
                         }
                     }
-                }catch(Exception eg)
-                {
-                    MessageBox.Show("Error", eg.ToString());
+                    catch (Exception eg)
+                    {
+                        MessageBox.Show("Error", eg.ToString());
+                    }
                 }
+                string[] row = new string[] { textBox2.Text, textBox5.Text, textBox3.Text, textBox6.Text, textBox4.Text };
+                dataGridView1.Rows.Add(row);
             }
+            listBox1.Items.Clear();
+            
         }
 
         private void Button3_Click(object sender, EventArgs e)
@@ -125,6 +147,11 @@ namespace ConvertPDF2TXT
             {
                 richTextBox1.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText);
             }
+        }
+
+        private void DataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
         }
     }
 }
